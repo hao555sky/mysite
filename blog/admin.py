dfe05bedc842395074285581066db6aea5d67484
hao555sky <836095186@qq.com>
2016-09-19 16:56:41 +0800

    add the html editor named tinymce

--- blog/admin.py
+++ blog/admin.py
@@ -10,7 +10,7 @@ admin.site.register(Category)
 # 添加Tinymce编辑器
 class BlogAdmin(admin.ModelAdmin):
     class Media:
-        js = ['blog/tinymce/js/tinymce/tinymce.min.js',
-              'blog/tinymce/config.js']
+        js = ['/static/tinymce/js/tinymce/tinymce.min.js',
+              '/static/tinymce/config.js']
 
 admin.site.register(Blog, BlogAdmin)
